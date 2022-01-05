95044f7d67985919d7110f0e1d7ce5116c7f6fa3
RushiBawake <rushi.bawake18@gmail.com>
2022-01-05 18:54:21 +0530

    Second Version

--- baba.py
+++ baba.py
@@ -1,32 +1,6 @@
+Great=input("Is India Great?")
 
-z=[]
-y=[]
-def Something(input1,input2,input3):
-    if input1%2==0:
-        for i in range(0,input1-1,2):
-            a=input2[i]+input2[i+1]
-            b=a*input3
-            y.append(b)
-            z.append(a)
-        c=sum(z)*input3
-        d=sum(y)
-        print(c+d)
-    elif input1%2==1:
-        for i in range(0,input1-2,2):
-            a=input2[i]+input2[i+1]
-            b=a*input3
-            y.append(b)
-            z.append(a)
-        e=(input2[input1-1]+input2[input1-1])*input3
-        c=sum(z)*input3
-        d=sum(y)
-        print(e+d)
-    
-if __name__=="__main__":
-
-    l=[2,3,1]
-    Something(3,l , 2)
-    """
-    l=[4,5,6,7]
-    Something(4,l , 3)
-    """
+if Great=="yes":
+    print("Yes! It is great")
+else:
+    print("HAHAHA India is still Great")
